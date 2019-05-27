d3c469c86cc1c2957be55e7f858efbc0b13633d1
Keenan Cahill <thatkeenancahill@gmail.com>
2019-05-27 18:36:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
-function dwarfRollCall(dwarves) { ["Doc", "Dopey", "Bashful", "Grumpy"]
+function dwarfRollCall(dwarves) {
+  let listDwarves = []
+  for(let i = 0; i < dwarves.length; i++){
+    
+    listDwarves.push(`${i+1}. ${dwarves[i]} `)
+    
+  }
+  return listDwarves.join(""); //The join() method joins all elements of an array (or an array-like object) into a string and returns this string.
 }
 
-function summonCaptainPlanet(planeteerCalls){ planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
+
+function summonCaptainPlanet(planeteerCalls){
+  let uppercasePlaneteerCalls = [];
+  for(let i = 0; i < planeteerCalls.length; i++){
+    let punctuation = "!"
+    uppercasePlaneteerCalls.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
+  }
+  return uppercasePlaneteerCalls
 }
+
+
 
 function longPlaneteerCalls(words) {
-  shortCalls = ["wind", "fire"]
-  longPlaneteerCalls(short_words); false
-  longcalls = ["earth", "wind", "heart", "fire"]
-  longPlaneteerCalls(assorted_words); true
+  
+  for(let i = 0; i < words.length; i++){
+    
+    if(words[i].length>4){
+      return true
+    }
+  }
+  return false
 }
 
-function findTheCheese (foods) { 
- snacks = ["crackers", "gouda", "thyme"]
-  findTheCheese(snacks); "gouda"
-  soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-  findTheCheese(soup); "cheddar"
-  ingredients = ["garlic", "rosemary", "bread"]
-  findTheCheese(ingredients); null
+
+
+function findTheCheese (foods) {
+  
+  for (var i = 0; i < foods.length; i++) {
+    
+    if (foods[i] ===  'gouda'|| foods[i]===  'cheddar' || foods[i]==='camembert') {
+      return foods[i];
+    }
+  }
+  return 'no cheese!'
 }
