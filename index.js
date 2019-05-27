1662e66baae5d79b78735227413b2cd34af556b8
Keenan Cahill <thatkeenancahill@gmail.com>
2019-05-27 18:33:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-function dwarfRollCall(dwarves) {
+function dwarfRollCall(dwarves) { ["Doc", "Dopey", "Bashful", "Grumpy"]
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls){ planeteerCalls = ["earth", "wind", "fire", "water", "heart"]
 }
 
 function longPlaneteerCalls(words) {
+  shortCalls = ["wind", "fire"]
+  longPlaneteerCalls(short_words); false
+  longcalls = ["earth", "wind", "heart", "fire"]
+  longPlaneteerCalls(assorted_words); true
 }
 
-function findTheCheese (foods) {
+function findTheCheese (foods) { 
+ snacks = ["crackers", "gouda", "thyme"]
+  findTheCheese(snacks); "gouda"
+  soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+  findTheCheese(soup); "cheddar"
+  ingredients = ["garlic", "rosemary", "bread"]
+  findTheCheese(ingredients); null
 }
